3d020b555346053df56024e9254f451676c08172
senabusegurocak <senabuseg@gmail.com>
2023-08-18 14:51:20 +0300

    second way

--- randomColor/randomColor.js
+++ randomColor/randomColor.js
@@ -1,7 +1,7 @@
 const random = document.querySelector(".randombtn")
 const box = document.querySelector(".box")
 
-function produce() {
+/*function produce() {
     var color1 = Math.floor(Math.random()*256);
     var color2 = Math.floor(Math.random()*256);
     var color3 = Math.floor(Math.random()*256);
@@ -9,6 +9,21 @@ function produce() {
     console.log(color);
     box.style.backgroundColor = color;
 }
+*/
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*256)
+}
+function produce() {
+    let r = getRandomNumber();
+    let g = getRandomNumber();
+    let b = getRandomNumber();
+    let color = 'rgb(' + r + "," + g + "," + b + ')';
+    console.log(color);
+    box.style.backgroundColor = color;
+    return color
+}
+
 random.addEventListener ("click", ()=> {
-    produce()
+    produce();
 })
